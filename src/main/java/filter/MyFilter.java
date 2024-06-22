0c8a2bee108a265f53c027907bb2270f8e21ff14
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-22 16:10:00 -0300

    finalização de bug fixes

--- src/main/java/filter/MyFilter.java
+++ src/main/java/filter/MyFilter.java
@@ -1,46 +1,28 @@
 package filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
 
-import javax.servlet.*;
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code
+    }
 
-import connection.SingleConnection;
-import jakarta.servlet.annotation.WebFilter;
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // Filtering code
+        chain.doFilter(request, response);
+    }
 
-@WebFilter(urlPatterns = {"/*"})
-public class MyFilter implements javax.servlet.Filter{
-
-	private static Connection connection;
-
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
-			throws IOException, ServletException {
-		try {
-			arg2.doFilter(arg0,arg1);
-			connection.commit();
-		}catch(Exception e){
-			try {
-				connection.rollback();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-
-
-	}
-
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		connection = SingleConnection.getConnection();
-
-	}
-
+    @Override
+    public void destroy() {
+        // Cleanup code
+    }
 }
