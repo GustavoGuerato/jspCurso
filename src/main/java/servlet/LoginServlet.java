41046d2e98cf1e57f1b8298f20b8862efa2384ca
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-24 14:16:49 -0300

    possibilidade de verificacao do login

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -9,25 +9,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import beans.BeansCursoJsp;
+import dao.DaoLogin;
 
 /**
  * Servlet implementation class LoginServlet
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
+	private DaoLogin daoLogin = new DaoLogin();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public LoginServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -41,18 +38,23 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
+		try {
+		
 		BeansCursoJsp beansCursoJsp = new BeansCursoJsp();
 
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 
-		if (beansCursoJsp.validarLoginSenha(login, senha)) {
+		if (daoLogin.validarLogin(login, senha)) {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("acessoLiberado.jsp");
 			dispatcher.forward(request, response);
 		} else {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
 			dispatcher.forward(request, response);
+		}
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 
 	}
