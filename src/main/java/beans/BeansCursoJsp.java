417864ce70593a7ab834300834a1d6727ea19e62
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-07-26 00:42:29 -0300

    some changes

--- src/main/java/beans/BeansCursoJsp.java
+++ src/main/java/beans/BeansCursoJsp.java
@@ -1,5 +1,7 @@
 package beans;
 
+import java.util.Objects;
+
 public class BeansCursoJsp {
 	// Outros campos existentes
 	private Long id;
@@ -121,4 +123,34 @@ public class BeansCursoJsp {
 	public void setImagem(String imagem) {
 		this.imagem = imagem;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Pdf, bairro, cep, cidade, estado, ibge, id, imagem, login, nome, rua, senha, tel);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BeansCursoJsp other = (BeansCursoJsp) obj;
+		return Objects.equals(Pdf, other.Pdf) && Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(estado, other.estado)
+				&& Objects.equals(ibge, other.ibge) && Objects.equals(id, other.id)
+				&& Objects.equals(imagem, other.imagem) && Objects.equals(login, other.login)
+				&& Objects.equals(nome, other.nome) && Objects.equals(rua, other.rua)
+				&& Objects.equals(senha, other.senha) && Objects.equals(tel, other.tel);
+	}
+
+	@Override
+	public String toString() {
+		return "BeansCursoJsp [id=" + id + ", login=" + login + ", senha=" + senha + ", nome=" + nome + ", tel=" + tel
+				+ ", cep=" + cep + ", rua=" + rua + ", bairro=" + bairro + ", cidade=" + cidade + ", estado=" + estado
+				+ ", ibge=" + ibge + ", imagem=" + imagem + ", Pdf=" + Pdf + "]";
+	}
+
 }
